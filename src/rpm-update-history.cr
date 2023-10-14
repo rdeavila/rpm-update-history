766c7862b905f552332fe0e22fb85bdc45acaa1f
Rodrigo de Avila <rdeavila@gmail.com>
2023-10-14 10:46:32 -0300

    Testing subcommands

--- src/rpm-update-history.cr
+++ src/rpm-update-history.cr
@@ -1,28 +1,43 @@
-require "helper"
 require "option_parser"
+require "transactions"
+require "helper"
 
 module Rpm::Update::History
   VERSION = "0.1.0"
-  options = {} of Symbol => Bool
+  used_subcommand = false
 
   parser = OptionParser.new do |opts|
-    opts.on("-b", "--build", "Compile history info") { options[:build] = true }
-    opts.on("-g", "--graph", "Create statistic graphs") { options[:graph] = true }
+    opts.banner = "Usage: #{PROGRAM_NAME} [subcommand]"
+
+    opts.on("-b", "--build", "Compile history info") do
+      used_subcommand = true
+      Helper.dnf_installed?
+      puts Transactions.list_transactions
+    end
+
+    opts.on("-g", "--graph", "Create statistic graphs") do
+      used_subcommand = true
+    end
+
+    opts.on("-l", "--list", "List compiled statistics")  do
+      used_subcommand = true
+    end
+
+    opts.on("-u", "--upload", "Upload data to a central repository")  do
+      used_subcommand = true
+    end
+
     opts.on("-h", "--help", "Prints this help") do
+      used_subcommand = true
       puts opts
       exit
     end
-    opts.on("-l", "--list", "List compiled statistics") { options[:list] = true }
-    opts.on("-u", "--upload", "Upload data to a central repository") { options[:upload] = true }
   end
 
   parser.parse
 
-  unless Helper.binary_installed? "dnf"
-    STDERR.puts "dnf is not installed. Exiting."
+  unless used_subcommand
+    puts parser
     exit 1
   end
-
-  puts parser
-  exit 1
 end
