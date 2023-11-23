7e38d748234e8361219c0140e589aeb175826178
Rodrigo de Avila <rdeavila@gmail.com>
2023-11-22 21:11:37 -0300

    Implement new options

--- src/main.cr
+++ src/main.cr
@@ -1,42 +1,42 @@
-require "dotenv"
 require "helper"
 require "option_parser"
 require "transactions"
 
 module Rpm::Update::History
-  VERSION = "0.1"
-
-  pm = Helper.package_binary
-  Dotenv.load
-  Helper.package_manager_installed? pm
-  Helper.db_check
+  VERSION        = "23.11.1"
+  CONFIG         = Helper.load_config
+  PACKAGE_BINARY = Helper.package_binary
   used_subcommand = false
+
+  Helper.db_check
 
   parser = OptionParser.new do |opts|
     opts.banner = "Usage: #{PROGRAM_NAME} [subcommand]"
 
     opts.on("-b", "--build", "Compile history info") do
       used_subcommand = true
-      Transactions.build pm
+      Transactions.build PACKAGE_BINARY
     end
 
     # opts.on("-g", "--graph", "Create statistic graphs") do
     #   used_subcommand = true
     # end
 
-    # opts.on("-l", "--list", "List compiled statistics")  do
-    #   used_subcommand = true
-    # end
+    opts.on("-l", "--list", "List all compiled statistics") do
+      used_subcommand = true
+      Transactions.list
+    end
 
     # opts.on("-u", "--upload", "Upload data to a central repository")  do
     #   used_subcommand = true
     # end
 
-    # opts.on("-v", "--version", "Show version number")  do
-    #   used_subcommand = true
-    # end
+    opts.on("-v", "--version", "Show version number") do
+      used_subcommand = true
+      puts "RPM Update History version #{VERSION}"
+    end
 
-    opts.on("-h", "--help", "Prints this help") do
+    opts.on("-h", "--help", "You know what this option does") do
       used_subcommand = true
       puts opts
       exit
